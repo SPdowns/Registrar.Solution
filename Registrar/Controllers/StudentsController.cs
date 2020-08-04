46151b91541235a11e0571959086d63bcec4008f
Megan Hepner <megan.hepner@gmail.com>
2020-08-04 12:04:25 -0700

    begin Create route in controller and Create View
    <chesnekov@gmail.com>
    <blacchoy@gmail.com>
    <spdowns@protonmail.com>

--- Registrar/Controllers/StudentsController.cs
+++ Registrar/Controllers/StudentsController.cs
@@ -9,22 +9,30 @@ namespace Registrar.Controllers
 {
   public class StudentsController : Controller
   {
-  private readonly RegistrarContext _db;
-  
-  public StudentsController(RegistrarContext db)
-  {
-    _db = db;
-  }
+    private readonly RegistrarContext _db;
+    
+    public StudentsController(RegistrarContext db)
+    {
+      _db = db;
+    }
 
-  public ActionResult Index()
-  {
-    return View(_db.Students.OrderBy(student => student.StudentId).ToList());
-  }
+    public ActionResult Index()
+    {
+      return View(_db.Students.OrderBy(student => student.StudentId).ToList());
+    }
 
-  // public ActionResult Create()
-  // {
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Student student)
+    {
+      _db.Students.Add(student);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-  //   return View();
-  // }
+
   }
 }
