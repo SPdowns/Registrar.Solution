24a023e83da4e45ff3224b5fd164af66c2c1ceb3
Megan Hepner <megan.hepner@gmail.com>
2020-08-04 11:39:57 -0700

    create student index route and view
    <chesnekov@gmail.com>
    <blacchoy@gmail.com>
    <spdowns@protonmail.com>

---
+++ Registrar/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Registrar.Models;
+using System.Collections.Generic;
+
+namespace Registrar.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
